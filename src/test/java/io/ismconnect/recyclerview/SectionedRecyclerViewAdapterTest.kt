606a160eab8b1befc9b7ddd1d654d3ab501eed0e
Kel Bucey <kabucey@gmail.com>
2018-02-14 17:15:24 -0500

    Simplified package names.

--- src/test/java/io/ismconnect/recyclerview/SectionedRecyclerViewAdapterTest.kt
+++ src/test/java/io/ismconnect/recyclerview/SectionedRecyclerViewAdapterTest.kt
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-package io.ismconnect.recyclerview.sectioned
+package io.ismconnect.recyclerview
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
